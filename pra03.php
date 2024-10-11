908b1266b1bfa9ea8a4479690b0273ea3bfda6c4
bearapo <bearapo945@gmail.com>
2024-10-11 13:55:22 +0800

    程式基礎教材

---
+++ pra03.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>document</title>
+
+    <style>
+           *{font-family:'courier New',Courier,monospace;
+           }
+
+    </style>
+
+</head>
+<body>
+
+<h2> 正直角三角形 </h2>
+ <?php 
+ 
+
+
+for($i=0; $i<5 ; $i++){
+    for($j=0; $j<=$i ; $j++){
+    echo "*";
+    }
+    echo "<br>";
+}
+
+echo "<hr>";
+
+for($i=0; $i<5 ; $i++){
+    for($j=0; $j<($i+1) ; $j++){
+    echo "*";
+    }
+    echo "<br>";
+}
+
+echo "<hr>";
+echo "<h2>倒直角三角形</h2>";
+
+for($i=5; $i>0 ; $i--){
+    for($j=0; $j<$i ; $j++){
+    echo "*";
+    }
+    echo "<br>";
+}
+?>
+
+
+<h2> 正直角三角形 </h2>
+ <?php 
+ 
+
+for($i=0; $i<5 ; $i++){
+
+    for($k=0; $k<4-$i ; $k++){
+        echo "&nbsp;";
+    }
+
+    for($j=0; $j<(2*$i+1) ; $j++){
+    echo "*";
+    }
+    echo "<br>";
+}
+
+?>
+
+<h2> 正直角三角形 </h2>
+ <?php 
+ 
+
+for($i=0; $i<5 ; $i++){
+
+    for($k=0; $k<4-$i ; $k++){
+        echo "&nbsp;";
+    }
+
+    for($j=0; $j<(2*$i+1) ; $j++){
+    echo "*";
+    }
+    echo "<br>";
+}
+
+?>
+
+
+
+
+<h2> 倒正三角形 -改外圈，內圈不變 </h2>
+ <?php 
+ 
+
+for($i=4; $i>=0 ; $i--){
+ 
+    for($k=0; $k<4-$i ; $k++){
+        echo "&nbsp;";
+    }
+
+   
+    for($j=0; $j<(2*$i+1) ; $j++){
+    echo "*";
+    }
+    echo "<br>";
+}
+
+?>
+
+
+<h2> 倒正三角形-改內圈，不改外圈</h2>
+ <?php 
+ 
+
+for($i=0; $i<5 ; $i++){
+
+    for($k=0; $k<$i ; $k++){
+        echo "&nbsp;";
+    }
+
+    for($j=0; $j<(((4-$i)*2)+1) ; $j++){
+    echo "*";
+    }
+    echo "<br>";
+}
+
+
+?>
+
+
+<h2> 菱形 </h2>
+ <?php 
+ 
+for($i=0; $i<9 ; $i++){
+    if($i>4){
+        $k1=$i-4;
+        $j1=2*($i-(2*($i-4)))+1;
+    }else{
+        $k1=4-$i;
+        $j1=(2*$i+1);
+    }
+
+        for($k=0; $k<$k1 ; $k++){
+            echo "&nbsp;";
+        }
+       
+        for($j=0; $j<$j1; $j++){
+        echo "*";
+        }
+        echo "<br>";      
+}
+
+?>
+
+
+
+
+</body>
+</html>
